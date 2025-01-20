08fd7cfec01d8f1ce79604620e9c7455683bdc8d
marco-de-pietri <marco.deppo@hotmail.it>
2025-01-20 12:04:26 +0100

    initial commit

---
+++ src/cdgs_tools/cdgs_class.py
@@ -0,0 +1,304 @@
+"""
+module for cdgs class
+"""
+
+import copy
+import re
+import math
+import sys
+import pyvista as pv
+
+from cdgs_node_class import CDGSCylinderNode
+
+
+def get_cylinder_geometry(text):
+    """
+    this function parse the text of the cdgs file to extract the node geometry
+    at the moment it assumes we are working with a cylinder geometry
+    """
+
+    geometry_pat = re.compile(
+        r"^mesh_type.{1,}?(?=^source_data)", re.IGNORECASE | re.MULTILINE | re.DOTALL
+    )
+    check_geo_1_pat = re.compile(r"^mesh_type\s+.*", re.IGNORECASE | re.MULTILINE)
+    check_geo_2_pat = re.compile(r"^mesh_boundaries\s+.*", re.IGNORECASE | re.MULTILINE)
+
+    geom_text = geometry_pat.findall(text)
+    check_geo_1_line = check_geo_1_pat.findall(geom_text[0])
+    check_geo_2_line = check_geo_2_pat.findall(geom_text[0])
+
+    if check_geo_1_line[0].split()[1] != "cyl":
+        print("error mesh is not cyl!")
+        sys.exit()
+
+    if (
+        (check_geo_2_line[0].split()[1] != "2")
+        and (check_geo_2_line[0].split()[2] != "2")
+        and (check_geo_2_line[0].split()[3] != "2")
+    ):
+        print("error with mesh boundaries")
+        sys.exit()
+
+    geometry_lines = geom_text[0].splitlines()
+
+    basis = [float(value) for value in geometry_lines[2].split()]
+    axis = [float(value) for value in geometry_lines[3].split()]
+
+    # norm = pow(sum([pow(val,2) for val in axis]),0.5)
+
+    radius = float(geometry_lines[5].split()[1])
+    length = float(geometry_lines[7].split()[1])
+
+    return basis, axis, radius, length
+
+
+def get_node_intensity(text):
+    """
+    function that parse the cdgs text to extract the intensity of the cdgs node
+    """
+
+    pat_intensity = re.compile(r"^total_source\s+.*", re.IGNORECASE | re.MULTILINE)
+
+    intensity_line = pat_intensity.findall(text)
+
+    return float(intensity_line[0].split()[1])
+
+
+def get_mesh_id(text):
+    """
+    function that parse the cdgs text to extract the mesh id
+    """
+
+    pat_mesh_id = re.compile(r"^mesh_id\s+.*", re.IGNORECASE | re.MULTILINE)
+
+    mesh_line = pat_mesh_id.findall(text)
+
+    return int(mesh_line[0].split()[1])
+
+
+def get_energy_bin_vals(text):
+    """
+    function that parse the cdgs text to extract the energy bin values
+    """
+    pat_ebin_vals = re.compile(
+        r"^source_data.{1,}?(?=\Z)", re.IGNORECASE | re.MULTILINE | re.DOTALL
+    )
+
+    ebin_val_lines = pat_ebin_vals.findall(text)[0].splitlines()[3:]
+
+    ebin_val_list = []
+
+    for line in ebin_val_lines:
+        values = line.split()
+        for val in values:
+            ebin_val_list.append(float(val))
+    return ebin_val_list
+
+
+def get_energy_bins(text):
+    """
+    function that parse the cdgs text to extract the energy boundaries
+    """
+
+    pat_n_ebins = re.compile(r"^energy_boundaries\s+.*", re.IGNORECASE | re.MULTILINE)
+
+    pat_ebins = re.compile(
+        r"^energy_boundaries.{1,}?(?=^mesh_type)",
+        re.IGNORECASE | re.MULTILINE | re.DOTALL,
+    )
+
+    n_ebins_line = pat_n_ebins.findall(text)
+    ebins_lines = pat_ebins.findall(text)[0].splitlines()[1:]
+    eb_list = []
+    for line in ebins_lines:
+        values = line.split()
+        for val in values:
+            eb_list.append(float(val))
+
+    n_ebins = int(n_ebins_line[0].split()[1])
+
+    if len(eb_list) != n_ebins:
+        print("mismatch number of energy boundaries!")
+        sys.exit()
+
+    return eb_list
+
+
+def get_file_tot_intesity(path):
+    """
+    parse the text file to get the total intensity listed in the file header
+    """
+
+    rf = open(path, "r", encoding="utf8", errors="ignore")
+    for _, line in enumerate(rf):
+        if line.split()[0] == "global_source":
+            tot_intensity = float(line.split()[1])
+            break
+
+    return tot_intensity
+
+
+def get_file_mesh_n(path):
+    """
+    parse the text file to get the number of cdgs nodes
+    """
+    rf = open(path, "r", encoding="utf8", errors="ignore")
+    for _, line in enumerate(rf):
+        if line.split()[0] == "num_meshes":
+            tot_mesh = int(line.split()[1])
+            break
+    return tot_mesh
+
+
+def read_cdgs_file(cdgs_file: str) -> object:
+    """
+    read the cdgs file and return a CDGS object
+    """
+    block_pat = re.compile(
+        "(?=^mesh_id).{1,}?(?=^mesh_id|^end_source_data)",
+        re.IGNORECASE | re.MULTILINE | re.DOTALL,
+    )
+
+    cdgs_nodes_number = get_file_mesh_n(cdgs_file)
+    mesh_tot_intensity = get_file_tot_intesity(cdgs_file)
+
+
+    try:
+        imp_file = open(cdgs_file, "r", encoding="utf8", errors="ignore")
+    except IOError:
+        print("couldn't open DGS file")
+        sys.exit()
+    with imp_file:
+
+        cdgs_object = CDGS()
+
+        text = imp_file.read()
+        node_blocks = block_pat.findall(text)
+
+        for node_text in node_blocks:
+
+            cyl_node = CDGSCylinderNode()
+            mesh_id = get_mesh_id(node_text)
+            cyl_node.mesh_id = mesh_id
+
+            node_intensity = get_node_intensity(node_text)
+            cyl_node.node_intensity = node_intensity
+
+            energy_bins = get_energy_bins(node_text)
+            cyl_node.energy_bins = energy_bins
+
+            energy_bin_vals = get_energy_bin_vals(node_text)
+            cyl_node.energy_bin_vals = energy_bin_vals
+
+
+            basis, axis, radius, height = get_cylinder_geometry(node_text)
+            cyl_node.basis = basis
+            cyl_node.axis = axis
+            cyl_node.radius = radius
+            cyl_node.height = height
+
+            cyl_node.calculate_volume()
+            cyl_node.calculate_lateral_surface_area()
+            cyl_node.calculate_end_point()
+
+            cdgs_object.add_node(cyl_node)
+
+
+        if cdgs_object.tot_meshes != cdgs_nodes_number:
+            print("mismatch number of nodes!")
+            print ("nodes parsed: ", cdgs_object.tot_meshes)
+            print ("nodes in file: ", cdgs_nodes_number)
+            sys.exit()
+        if not math.isclose(cdgs_object.tot_intensity, mesh_tot_intensity, rel_tol=1e-6):
+            print("mismatch total intensity!")
+            print(f"intensity parsed: {cdgs_object.tot_intensity:.8e}")
+            print (f"intensity in file header: , {mesh_tot_intensity:.8e}")
+            sys.exit()
+
+    return cdgs_object
+
+
+class CDGS:
+    """
+    class for cdgs
+    """
+
+    def __init__(self) -> None:
+        self._tot_meshes = 0
+        self._tot_intensity = 0
+        self._tot_volume = 0
+        self._nodes = []
+
+    def __str__(self) -> str:
+        return (
+            f"CDGS object\n"
+            f"PROPERTY: \n"
+            f"total meshes {self.tot_meshes} \n"
+            f"total intensity [Bq] {self.tot_intensity:.2e}"
+            f"total volume [cm3] {self.tot_volume:.2e}"
+        )
+
+    @property
+    def nodes(self) -> list:
+        """
+        Returns the list of nodes.
+        """
+        return self._nodes
+
+    @property
+    def tot_meshes(self) -> int:
+        """
+        Returns the total number of meshes.
+        """
+        return self._tot_meshes
+
+    @property
+    def tot_intensity(self) -> float:
+        """
+        Returns the total intensity.
+        """
+        return self._tot_intensity
+
+    @property
+    def tot_volume(self) -> float:
+        """
+        Returns the total volume
+        """
+        return self._tot_volume
+
+
+    def add_node(self, node) -> None:
+        """
+        Add a node to the list of nodes.
+        """
+        self._nodes.append(copy.deepcopy(node))
+        self._tot_meshes += 1
+        self._tot_intensity += node.node_intensity
+        self._tot_volume += node.node_volume
+
+    def renumber_nodes(self) -> None:
+        """
+        renumber the nodes in the cdgs object
+        """
+        for i, node in enumerate(self.nodes):
+            node.mesh_id = i
+
+    def filter_by_stl (self, stl_file) -> object:
+        """
+        filter the cdgs object leaving only the nodes that are inside the stl file
+        """
+
+        mesh = pv.read(stl_file)
+        cdgs_subset = CDGS()
+
+        for node in self.nodes:
+
+            if node.is_inside_stl( mesh):
+                cdgs_subset.add_node(node)
+
+        cdgs_subset.renumber_nodes()
+
+        return cdgs_subset
+
+
+
---
+++ src/cdgs_tools/cdgs_node_class.py
@@ -0,0 +1,215 @@
+"""
+Module for the CDGS node class.
+"""
+import math
+import pyvista as pv
+from vtk import vtkSelectEnclosedPoints
+import numpy as np
+
+
+class CDGSNode:
+    """
+    class for cdgs node
+    """
+
+    class CDGSNode:
+        """
+        CDGS node generic class.
+        """
+        def __init__(self) -> None:
+            self._mesh_id = 0
+            self._energy_bins = []
+            self._energy_bin_vals = []
+            self._node_intensity = 0
+
+        @property
+        def mesh_id(self) -> int:
+            """
+            Returns the mesh ID.
+            """
+            return self._mesh_id
+
+        @mesh_id.setter
+        def mesh_id(self, value: int) -> None:
+            self._mesh_id = value
+
+        @property
+        def node_intensity(self) -> float:
+            """
+            Returns the intensity of the node.
+            """
+            return self._node_intensity
+
+        @node_intensity.setter
+        def node_intensity(self, value: float) -> None:
+            self._node_intensity = value
+
+        def __str__(self) -> str:
+            return (
+                f"CDGSNode object with mesh id {self.mesh_id} "
+                f"and node intensity {self.node_intensity:.2e}"
+            )
+class CDGSCylinderNode(CDGSNode):
+    """
+    class for cdgs cylinder node
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        self._basis = []
+        self._end_point = []
+        self._axis = []
+        self._radius = 0
+        self._height = 0
+        self._node_volume = 0
+        self._lateral_surface_area = 0
+
+    @property
+    def basis(self) -> list:
+        """
+        Returns the basis list.
+
+        :return: A list representing the basis.
+        :rtype: list
+        """
+        return self._basis
+
+    @basis.setter
+    def basis(self, value: list) -> None:
+        self._basis = value
+
+    @property
+    def end_point(self) -> list:
+        """
+        Returns the end point list.
+
+        :return: A list representing the end point.
+        :rtype: list
+        """
+        return self._end_point
+
+    def calculate_end_point(self) -> None:
+        """
+        Calculates the end point of the cylinder.
+        """
+        self._end_point = [
+            self.basis[0] + self.axis[0] * self.height,
+            self.basis[1] + self.axis[1] * self.height,
+            self.basis[2] + self.axis[2] * self.height,
+        ]
+
+    @property
+    def axis(self) -> list:
+        """
+        Returns the axis attribute.
+
+        Returns:
+            list: The axis attribute.
+        """
+        return self._axis
+
+    @axis.setter
+    def axis(self, value: list) -> None:
+        self._axis = value
+
+    @property
+    def radius(self) -> float:
+        """
+        Returns the radius of the node.
+
+        Returns:
+            float: The radius of the node.
+        """
+        return self._radius
+
+    @radius.setter
+    def radius(self, value: float) -> None:
+        self._radius = value
+
+    @property
+    def height(self) -> float:
+        """
+        Returns the length of the object.
+
+        Returns:
+            float: The length of the object.
+        """
+        return self._height
+
+    @height.setter
+    def height(self, value: float) -> None:
+        self._height = value
+
+    @property
+    def node_volume(self) -> float:
+        """
+        Returns the volume of the cylinder.
+
+        Returns:
+            float: The volume of the cylinder.
+        """
+        return self._node_volume
+
+    def calculate_volume(self) -> None:
+        """
+        Calculates the volume of the cylinder.
+        """
+        self._node_volume = math.pi * self.height * (self.radius ** 2)
+
+    @property
+    def lateral_surface_area(self) -> float:
+        """
+        Returns the lateral surface area of the cylinder.
+
+        Returns:
+            float: The lateral surface area of the cylinder.
+        """
+        return self._lateral_surface_area
+
+    def calculate_lateral_surface_area(self) -> None:
+        """
+        Calculates the lateral surface area of the cylinder.
+        """
+        self._lateral_surface_area = 2 * math.pi * self.radius * self.height
+
+    def is_inside_stl(self, stl_mesh: object) -> bool:
+        """
+        Checks if the cylinder is inside the STL file.
+
+        Args:
+            stl_file (str): The STL file.
+
+        Returns:
+            bool: True if the cylinder is inside the STL file, False otherwise.
+        """
+
+        check_points = [self.basis, self.end_point]
+
+        points_poly = pv.PolyData(check_points)
+
+        enclosed_points = vtkSelectEnclosedPoints()
+        enclosed_points.SetTolerance(0.000001)
+        enclosed_points.SetInputData(points_poly)
+        enclosed_points.SetSurfaceData(stl_mesh)
+        enclosed_points.Update()
+        inside_filter = enclosed_points.GetOutput().GetPointData().GetArray("SelectedPoints")
+        inside_filter = np.array([inside_filter.GetValue(i) for i in range(inside_filter.GetNumberOfTuples())])
+        if not np.array_equal(inside_filter, [0, 0]):
+            return True
+        else:
+            return False
+
+    def __str__(self) -> str:
+        return (
+            f"CDGSCylinderNode object \n"
+            f"PROPERTIES:\n"
+            f"mesh id {self.mesh_id}\n"
+            f"intensity {self.node_intensity:.2e}\n"
+            f"basis {self.basis}\n"
+            f"end point {self.end_point}\n"
+            f"axis {self.axis}\n"
+            f"radius {self.radius}\n"
+            f"height {self.height}\n"
+            f"volume {self.node_volume:.2e}\n"
+            f"lateral surface area {self.lateral_surface_area:.2e}\n"
+        )
